a4f1a61d2cfb789281a7866106a912d6e7f6b153
eldadgishinge <e.gishinge@alustudent.com>
2023-07-06 11:12:51 +0200

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
 """
-    python script that returns TODO list progress for a given employee ID
+Module to track TODO list progress of an employee using a REST API.
 """
 import requests
 import sys
 
 
 def get_employee_todo_progress(employee_id):
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    """
+    Given an employee ID, prints information about his/her TODO list progress.
+    """
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
 
-    """
-        request user info by employee ID
-    """
     response = requests.get(user_url)
     user = response.json()
+
     response = requests.get(todos_url)
     todos = response.json()
-    """
-        convert json to dictionary
-    """
-    done_tasks = [todo for todo in todos if todo["completed"] == True]
+
+    done_tasks = [todo for todo in todos if todo.get('completed') == True]
     number_of_done_tasks = len(done_tasks)
     total_number_of_tasks = len(todos)
 
-    """
-        extract employee name
-    """
-    print(f"Employee {user['name']} is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):")
+    print("Employee {} is done with tasks({}/{}):".format(user.get('name'), number_of_done_tasks, total_number_of_tasks))
 
-    """
-        request user's TODO list
-    """
     for task in done_tasks:
-        print(f"\t {task['title']}")
-    """
-        dictionary to store task status in boolean format
-    """
-    if __name__ == "__main__":
-        get_employee_todo_progress(int(sys.argv[1]))
+        print("\t {}".format(task.get('title')))
+
+
+if __name__ == "__main__":
+    get_employee_todo_progress(int(sys.argv[1]))
+

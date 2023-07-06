5fc9570dbc172e2438bf35d08980e843210424be
eldadgishinge <e.gishinge@alustudent.com>
2023-07-06 10:56:36 +0200

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,23 +5,37 @@
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
+    """
+        request user info by employee ID
+    """
     response = requests.get(user_url)
     user = response.json()
     response = requests.get(todos_url)
     todos = response.json()
-
+    """
+        convert json to dictionary
+    """
     done_tasks = [todo for todo in todos if todo["completed"] == True]
     number_of_done_tasks = len(done_tasks)
     total_number_of_tasks = len(todos)
 
+    """
+        extract employee name
+    """
     print(f"Employee {user['name']} is done with tasks({number_of_done_tasks}/{total_number_of_tasks}):")
 
+    """
+        request user's TODO list
+    """
     for task in done_tasks:
         print(f"\t {task['title']}")
-
-if __name__ == "__main__":
-    get_employee_todo_progress(int(sys.argv[1]))
+    """
+        dictionary to store task status in boolean format
+    """
+    if __name__ == "__main__":
+        get_employee_todo_progress(int(sys.argv[1]))
